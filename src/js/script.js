a0ac173281d59109826345e75fb464473c9920c6
Fedor <deadkovalev@yandex.ru>
2020-04-21 02:21:13 +0300

    add slick-carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
-var name = "Fedor";
-let number = 7;
-const pi = 3.14;
-
-console.log(4+"4");
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow/arrow_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow/arrow_right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false,
+                }
+            }    
+        ]
+      });
+  });
